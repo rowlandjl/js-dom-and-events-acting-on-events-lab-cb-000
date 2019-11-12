45d69015cec05219b17c026106b2c6bfa54118bb
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-12 19:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ let input = document.querySelector('input');
 function retrieveEmployeeInformation() {
   return input.value;
 }
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation();
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`);
+}
